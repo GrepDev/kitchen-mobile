d08fba1a25a2be8d63404dfaf5d9d5bc61f5fceb
laur <laurentiu.raducu@gmail.com>
2018-09-23 10:39:33 +0300

    Adding the functionality for adding the query results to a list in the ingredients page + some fail handlers in case if user tries to input duplicate ingredients or no response is received from the edamam server.

--- src/pages/ingredients/ingredients.ts
+++ src/pages/ingredients/ingredients.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { EdamamApiProvider } from '../../providers/edamam-api/edamam-api';
-import { HttpClient } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
+
 
 /**
  * Generated class for the RegisterPage page.
@@ -17,37 +16,75 @@ import { HttpModule } from '@angular/http';
 })
 export class IngredientsPage {
 
-  private food;
+  private food = [];
 
-  public getFood(){
+  public getFood() {
     return this.food;
   }
 
-  public sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+  private foodID;
+
+  public getFoodID() {
+    return this.foodID;
   }
 
   private nullMessage: string = "Search for an ingredient first";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private edamamApiProvider:EdamamApiProvider) {
+  private nullIngredientID: string = "Ingredient ID cannot be null";
+
+  private duplicateIngredient: string = "The specified ingredient is already in your list"
+
+  private noResponseFromWebService: string = "There was an error retrieving the response from the webservice"
+
+  private noIngredientInTheList: string = "You currently don't have any ingredient added." +
+    "You can either search for the ingredients that you have in your kitchen, " +
+    "or those that you want to use in your recipe."
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private edamamApiProvider: EdamamApiProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IngredientsPage');
   }
 
-  getIngredients(ingredient){
-    //console.log("Interogating service");
+  getIngredients(queryText) {
+    return null;
   }
 
-  addIngredient(ingredient){ 
-    if(ingredient == null){
+  sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+  getIngredientsData(ingredient) {
+    if (ingredient == null) {
       document.getElementById("defaultList").innerHTML = this.nullMessage;
       return null;
     }
     this.edamamApiProvider.getIngredientsData(ingredient).subscribe(data => {
-      this.food = data;
-      document.getElementById("defaultList").innerHTML = this.food;
-    })
+      if (data == null) {
+        this.sleep(5000)
+      }
+      if (data == null) {
+        alert(this.noResponseFromWebService);
+      }
+      for (var i = 0; i < this.food.length; i++) {
+        if (this.food[i] == data) {
+          alert(this.duplicateIngredient);
+          return 0;
+        }
+      }
+      this.food.push(data);
+    }
+    )
+    document.getElementById("defaultList").style.visibility = "hidden";
+    document.getElementById("ingredientsContainer").style.visibility = "visible";
+  }
+
+  removeItem(item) {
+    for (var i = 0; i < this.food.length; i++) {
+      if (this.food[i] == item) {
+        this.food.splice(i, 1);
+      }
+    }
   }
 }
--- src/providers/edamam-api/edamam-api.ts
+++ src/providers/edamam-api/edamam-api.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { resolveDefinition } from '@angular/core/src/view/util';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
@@ -12,7 +10,7 @@ export class EdamamApiProvider {
   private ingredientsUrl = 'https://api.edamam.com/api/food-database/parser?app_key=d1bf31a95d67109d48fb02e4b81c3075&app_id=fc8fb72e&ingr=';
   private currentFood: any = {};
 
-  public getBaseUrl(){
+  public getBaseUrl() {
     return this.baseUrl;
   }
 
@@ -20,18 +18,19 @@ export class EdamamApiProvider {
     console.log('Data provider has been constructed');
   }
 
-  getIngredients(ingredients) {
-    let url = this.ingredientsUrl + encodeURI(ingredients); 
-    return new Promise(resolve => {
-        this.http.get(`${url}`).subscribe(res => resolve(res.json()));
-    });
-  }
-
-  getIngredientsData(ingredients) : Observable<any> {
-    let url = this.ingredientsUrl + encodeURI(ingredients); 
+  getIngredientsData(ingredients): Observable<any> {
+    let url = this.ingredientsUrl + encodeURI(ingredients);
     return this.http.get(`${url}`).map(response => {
       this.currentFood = response.json();
       return this.currentFood.parsed[0].food.label;
-    })  
+    })
+  }
+
+  getIngredientID(ingredientName): Observable<any> {
+    let url = this.ingredientsUrl + encodeURI(ingredientName);
+    return this.http.get(`${url}`).map(response => {
+      this.currentFood = response.json();
+      return this.currentFood.parsed[0].food.foodId;
+    })
   }
 }
