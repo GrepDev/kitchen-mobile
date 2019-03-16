56d0977b83ae02932dee75f179180f7384ad6fc6
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2019-03-16 19:10:54 +0200

    Update upload-apk.sh

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 mkdir $HOME/buildApk/
-cp -R /media/laur/FD_BETA9SR2/Personal/Projects/Edesia/kitchen-mobile/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $HOME/android/
+cp -R $HOME/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $HOME/android/
 cd $HOME
+echo $HOME
 git config --global user.email "laurentiu.raducu@gmail.com"
 git config --global user.name "LaurentiuGabriel"
-git clone --quiet --branch "feature/travis-deploy" = https://GrepDev:4b44633443ddf9bf2b63c7eb8335ff7faa836c1a@github.com/GrepDev//kitchen-mobile feature/travis-deploy > /dev/null
+git clone --quiet --branch "feature/travis-deploy" = https:/GrepDev:4b44633443ddf9bf2b63c7eb8335ff7faa836c1a@github.com/GrepDev/kitchen-mobile feature/travis-deploy > /dev/null
 git checkout --orphan temporary
 git add --all .
 git commit -am "[Auto] Update Test Apk ($(date +%Y-%m-%d.%H:%M:%S))"
