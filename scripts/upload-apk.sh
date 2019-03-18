992952587f96af7d454ae3dde2f345a442337a8c
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2019-03-18 10:45:53 +0200

    Update upload-apk.sh

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -5,8 +5,6 @@ cp -R $HOME/build/GrepDev/kitchen-mobile/platforms/android/app/build/outputs/apk
 cd $HOME
 git config --global user.email "laurentiu.raducu@gmail.com"
 git config --global user.name "LaurentiuGabriel"
-git clone --quiet --branch="feature/travis-deploy" git@github.com:GrepDev/kitchen-mobile.git
-
 yes | git clone --quiet --branch=feature/travis-deploy https://GrepDev:4b44633443ddf9bf2b63c7eb8335ff7faa836c1a@github.com/GrepDev/kitchen-mobile apk > /dev/null
 ls -l
 cd kitchen-mobile 
