f011c9f29b19491401d2b15f0e491f0ca4332872
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2019-03-24 19:40:56 +0200

    Update prepare-apk.sh

--- prepare-apk.sh
+++ prepare-apk.sh
@@ -2,6 +2,6 @@ mv /home/travis/build/GrepDev/kitchen-mobile/my-release-key.keystore /home/travi
 cd /home/travis/build/GrepDev/kitchen-mobile/platforms/android/app/build/outputs/apk/release/
 mv app-release-unsigned.apk edesia.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore edesia.apk app-release <<!
-$PASSPHRASE
-$PASSPHRASE
+$1
+$1
 !
