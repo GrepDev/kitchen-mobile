241ea98d50fb94b9f408ce66b69f810cf7a7f6dd
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2019-04-06 05:16:51 +0300

    Update conf.js

--- src/protractor/conf.js
+++ src/protractor/conf.js
@@ -15,10 +15,8 @@ exports.config = {
 
   chromeOptions: {
      args: [ "--headless", "--disable-gpu", "--window-size=800,600" ]
-   }
-}, 
-
-
+   }, 
+        
     onPrepare: function(){
         browser.driver.manage().window().setPosition(0,0);
         browser.driver.manage().window().setSize(1280,720);
