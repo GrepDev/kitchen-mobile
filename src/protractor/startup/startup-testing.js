4daeaba5eedb28b8467003a42b751f8e9a61db45
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2019-04-11 15:19:07 +0300

    Update startup-testing.js

--- src/protractor/startup/startup-testing.js
+++ src/protractor/startup/startup-testing.js
@@ -1,8 +1,6 @@
 describe('Startup testing: ', function(){
     describe('When the site loads up', function(){
-        it('Should have all images loaded', () => {
- jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
-}, function(){
+        it('Should have all images loaded', function(){
             browser.ignoreSynchronization = true;
             browser.get('http://10.20.0.134:8100');
             browser.getPageSource().then(function (res) {
